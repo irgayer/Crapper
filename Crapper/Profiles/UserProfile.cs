3468d30ec0f0ad9aa2e8d93563d25075127ab5f7
Yerbol Irgaliyev <yerbol.irgaliyev@gmail.com>
2022-05-11 09:57:06 +0600

    add automapper

---
+++ Crapper/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Crapper.DTOs.User;
+using Crapper.Models;
+
+namespace Crapper.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserRegistrationDto, User>();
+        }
+    }
+}
