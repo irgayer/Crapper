02278a1c10913bf4c13b8878d20f21d3107637b1
Yerbol Irgaliyev <yerbol.irgaliyev@gmail.com>
2022-05-11 07:18:33 +0600

    initial commit

---
+++ Crapper/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Crapper.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        IQueryable<T> GetAll();
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ Crapper/Models/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace Crapper.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public User Author { get; set; }
+        public int UserId { get; set; }
+    }
+}
