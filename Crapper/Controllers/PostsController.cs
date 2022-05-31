6538aaf62ea9044c39fe62ececa8ddf007033640
Yerbol Irgaliyev <yerbol.irgaliyev@gmail.com>
2022-05-31 21:41:11 +0600

    move posts logic to Commands and Queries

--- Crapper/Controllers/PostsController.cs
+++ Crapper/Controllers/PostsController.cs
@@ -1,12 +1,22 @@
 ﻿using AutoMapper;
 using Crapper.DTOs.Post;
+using Crapper.Features.PostsFeatures.Commands.AddPost;
+using Crapper.Features.PostsFeatures.Commands.DeletePost;
+using Crapper.Features.PostsFeatures.Queries.GetAllPosts;
+using Crapper.Features.PostsFeatures.Queries.GetPostsByFilter;
+using Crapper.Features.UserFeatures.Queries.GetUserById;
 using Crapper.Interfaces;
 using Crapper.Models;
+
+using MediatR;
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
+using System.Security.Claims;
 
 namespace Crapper.Controllers
 {
@@ -18,12 +28,14 @@ namespace Crapper.Controllers
         private readonly IRepository<Post> _postRepository;
         private readonly IRepository<User> _userRepository;
         private readonly IMapper _mapper;
+        private readonly ISender _mediator;
 
-        public PostsController(IRepository<Post> postRepository, IMapper mapper, IRepository<User> userRepository)
+        public PostsController(ISender mediator, IRepository<Post> postRepository, IMapper mapper, IRepository<User> userRepository)
         {
             _postRepository = postRepository;
             _mapper = mapper;
             _userRepository = userRepository;
+            _mediator = mediator;
         }
 
         [HttpPost]
@@ -31,40 +43,42 @@ namespace Crapper.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Create(PostCreateDto req)
         {
-            var post = _mapper.Map<Post>(req);
-            var user = _userRepository.Find(user => user.Username == User.Identity.Name).SingleOrDefault();
+            // todo: create better solution
+            var id = int.Parse(User.FindFirstValue("id"));
+            var user = await _mediator.Send(new GetUserByIdQuery(id));
 
             if (user == null)
                 return BadRequest();
 
-            post.AuthorId = user.Id;
-            await _postRepository.Add(post);
-            await _postRepository.Save();
+            var post = await _mediator.Send(new AddPostCommand(req, id));
+            if (post == null)
+                return BadRequest();
 
-            return Ok();
+            return Ok(post);
         }
 
         [HttpGet]
         [AllowAnonymous]
-        public ActionResult<IEnumerable<PostDto>> GetAll()
+        public async Task<ActionResult<IEnumerable<PostDto>>> GetAll()
         {
-            var posts = _postRepository.GetAll();
-            var res = _mapper.Map<IEnumerable<PostDto>>(posts.AsEnumerable());
+            var posts = await _mediator.Send(new GetAllPostsQuery());
 
-            return Ok(res);
+            return Ok(posts);
         }
 
         //todo: rewrite boilerplate, use GetByUser(id)
         [HttpGet("my")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult GetPostsByIdentity()
+        public async Task<IActionResult> GetPostsByIdentity()
         {
-            var user = _userRepository.Find(user => user.Username == User.Identity.Name).SingleOrDefault();
-            //var posts = user.Posts.AsEnumerable();
-            var posts = _postRepository.Find(x => x.AuthorId == user.Id).AsEnumerable();
-            var res = _mapper.Map<IEnumerable<PostDto>>(posts);
+            var id = int.Parse(User.FindFirstValue("id"));
+            var user = await _mediator.Send(new GetUserByIdQuery(id));
 
-            return Ok(res);
+            if (user == null)
+                return BadRequest();
+
+            var posts = await _mediator.Send(new GetPostsByFilterQuery(x => x.Author.Id == id));
+            return Ok(posts);
         }
 
         [HttpGet("user/{id}")]
@@ -73,15 +87,13 @@ namespace Crapper.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetByUserAsync(int id)
         {
-            var user = await _userRepository.GetById(id);
+            var user = await _mediator.Send(new GetUserByIdQuery(id));
             
             if (user == null)
                 return NotFound();
 
-            var posts = _postRepository.Find(post => post.AuthorId == user.Id);
-            var res = _mapper.Map<IEnumerable<PostDto>>(posts.AsEnumerable());
-
-            return Ok(res);
+            var posts = await _mediator.Send(new GetPostsByFilterQuery(x => x.Author.Id == id));
+            return Ok(posts);
         }
 
         [HttpDelete("{id}")]
@@ -90,18 +102,37 @@ namespace Crapper.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Delete(int id)
         {
-            var user = User.Identity.Name;
-            var post = _postRepository.Find(post => post.Id == id).SingleOrDefault();
+            // fix: rewrite this crazy bullshit.
+            var userId = int.Parse(User.FindFirstValue("id"));
+            var user = await _mediator.Send(new GetUserByIdQuery(userId));
 
-            if (post == null)
-                return NotFound();
-            if (user != post.Author.Username)
+            if (user == null)
                 return BadRequest();
 
-            _postRepository.Delete(post);
-            await _postRepository.Save();
+            var post = await _mediator.Send(new GetPostsByFilterQuery(x => x.Id == id));
+            if (!post.Any())
+                return NotFound();
 
-            return Ok();
+            if (user.Id != post.First().AuthorId)
+                return BadRequest();
+
+            await _mediator.Send(new DeletePostCommand(id));
+            return Ok(post);
+
+
+
+            //var user = User.Identity.Name;
+            //var post = _postRepository.Find(post => post.Id == id).SingleOrDefault();
+
+            //if (post == null)
+            //    return NotFound();
+            //if (user != post.Author.Username)
+            //    return BadRequest();
+
+            //_postRepository.Delete(post);
+            //await _postRepository.Save();
+
+            //return Ok();
         }
 
         [HttpPatch("{id}")]
---
+++ Crapper/Features/PostsFeatures/Commands/AddPost/AddPostCommand.cs
@@ -0,0 +1,19 @@
+﻿using Crapper.DTOs.Post;
+using Crapper.Models;
+
+using MediatR;
+
+namespace Crapper.Features.PostsFeatures.Commands.AddPost
+{
+    public class AddPostCommand : IRequest<PostDto>
+    {
+        public PostCreateDto Post { get; set; }
+        public int AuthorId { get; set; }
+
+        public AddPostCommand(PostCreateDto post, int authorId)
+        {
+            Post = post;
+            AuthorId = authorId;
+        }
+    }
+}
