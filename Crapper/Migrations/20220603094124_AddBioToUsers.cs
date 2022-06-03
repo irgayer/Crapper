3818bcfabbe7322d8ad2dd4201f773b1143df6ba
Yerbol Irgaliyev <yerbol.irgaliyev@gmail.com>
2022-06-04 04:10:22 +0600

    add bio column to user

---
+++ Crapper/Migrations/20220603094124_AddBioToUsers.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Crapper.Migrations
+{
+    public partial class AddBioToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "Users",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+        }
+    }
+}
--- Crapper/Models/User.cs
+++ Crapper/Models/User.cs
@@ -1,11 +1,22 @@
-﻿namespace Crapper.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crapper.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [StringLength(20, MinimumLength = 3)]
         public string Username { get; set; }
+
         public string Email { get; set; }
+        
+        [StringLength(300)]
+        public string? Bio { get; set; }
+
+        [StringLength(20, MinimumLength = 8)]
         public string Password { get; set; }
+
         public ICollection<Post> Posts { get; set; }
         
         public ICollection<Subscription> Subscribers { get; set; }
