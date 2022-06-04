46b5aa79ece2f549fbe4d4da7887fb4854ce6bc4
Marvin Brereton <marvin.brereton@gmail.com>
2022-06-04 14:41:16 +0100

    Added base entitity model

---
+++ Crapper/Models/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Crapper.Models
+{
+    /// <summary>
+    /// This model will be inherited by other classes, any objects added here will be carried across your models dependant on what type you pass in
+    /// </summary>
+    /// Clean install of DB entrees to reflect changes:
+    /// Step 1: Package manager console:  Update-database -TargetMigration:0 -f (This resets tables to the original migration when you started so theyre clear
+    /// Step 2: Update-database (This resets columns etc that you added and sets db to current code migration
+    /// <typeparam name="T"></typeparam>
+    public class BaseModel<T>
+    {
+        /// <summary>
+        /// This is generic Id, you can apply a type when inheriting this class
+        /// </summary>
+        public T Id { get; set; }
+    }
+}
