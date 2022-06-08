9cf9a634327b6780ef6ab28a5d9f9e2965b0e487
Yerbol Irgaliyev <yerbol.irgaliyev@gmail.com>
2022-06-08 17:45:04 +0600

    move auth logic to AuthController

---
+++ Crapper/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Crapper.DTOs.User;
+using Crapper.Features.UserFeatures.Commands.AddUser;
+using Crapper.Features.UserFeatures.Queries.LoginUser;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ISender _mediator;
+
+        public AuthController(ISender mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Register(UserRegistrationDto req)
+        {
+            var success = await _mediator.Send(new AddUserCommand(req));
+            if (!success)
+                return BadRequest();
+
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Login(UserLoginDto req)
+        {
+            var result = await _mediator.Send(new LoginUserQuery(req));
+            if (!result.Success)
+                return BadRequest();
+
+            return Ok(result.Jwt);
+        }
+    }
+}
--- Crapper/Controllers/UserController.cs
+++ Crapper/Controllers/UserController.cs
@@ -1,9 +1,6 @@
 ﻿using System.Security.Claims;
 
-using Crapper.DTOs.User;
-using Crapper.Features.UserFeatures.Commands.AddUser;
 using Crapper.Features.UserFeatures.Queries.GetUserById;
-using Crapper.Features.UserFeatures.Queries.LoginUser;
 
 using MediatR;
 
@@ -24,18 +21,6 @@ namespace Crapper.Controllers
             _mediator = mediator;
         }
 
-        [HttpPost("register")]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        public async Task<IActionResult> Register(UserRegistrationDto req)
-        {
-            var success = await _mediator.Send(new AddUserCommand(req));
-            if (!success)
-                return BadRequest();
-
-            return Ok();
-        }
-
         [HttpGet("whoami")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
@@ -45,18 +30,6 @@ namespace Crapper.Controllers
             var user = await _mediator.Send(new GetUserByIdQuery(id));
 
             return Ok(new { id = user.Id });         
-        }
-
-        [HttpPost("login")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Login(UserLoginDto req)
-        {        
-            var result = await _mediator.Send(new LoginUserQuery(req));
-            if (!result.Success)
-                return BadRequest();
-
-            return Ok(result.Jwt);
         }
 
         [HttpGet("{id}")]
